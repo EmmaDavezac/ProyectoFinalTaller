a4623557ed0bc391214244669877166c5bb1b19a
Emmanuel Davezac <89666361+EmmaDavezac@users.noreply.github.com>
2022-01-05 15:05:25 -0300

    Update Libro.cs
    Comentado pero con dudas

--- Dominio/Libro.cs
+++ Dominio/Libro.cs
@@ -2,23 +2,23 @@
 
 namespace Dominio
 {
-    public class Libro
+    public class Libro //Esta clase es una abstracciòn de un libro
     {
-        public int Id { get; set; }
-        public string ISBN { get; set; }
-        public string Titulo { get; set; }
-        public string Autor { get; set; }
-        public string AñoPublicacion { get; set; }
-        public virtual List<Ejemplar> Ejemplares { get; set; }
-        public bool Baja { get; set; }
+        public int Id { get; set; }//Clave primaria que nos permite diferenciar los libros entre si
+        public string ISBN { get; set; }//Codigo ISBN del libro (en el caso de que lo tenga)
+        public string Titulo { get; set; }//Titulo del libro
+        public string Autor { get; set; }//Nombre del autor del libro
+        public string AñoPublicacion { get; set; }//Año de publicaciòn del libro
+        public virtual List<Ejemplar> Ejemplares { get; set; }//Lista de ejemplares del libro
+        public bool Baja { get; set; }//Property que nos permite dar una baja logica al libro
 
 
-        public Libro()
+        public Libro()//Constructor de la clase sin argumentos 
         {
 
         }
 
-        public Libro(string unISBN, string titulo, string autor, string añoPublicacion)
+        public Libro(string unISBN, string titulo, string autor, string añoPublicacion)//Constructor de la clase
         {
             ISBN = unISBN;
             Titulo = titulo;
@@ -27,18 +27,18 @@ namespace Dominio
             Baja = false;
         }
 
-        public List<Ejemplar> EjemplaresDisponibles()
+        public List<Ejemplar> EjemplaresDisponibles() //Metodo que nos devuelve la lista de ejemplares del libro que se encuentran disponibles para prestarse actualmente
         {
-            List<Ejemplar> ejemplaresDisponibles = new List<Ejemplar>();
-            foreach (var item in this.Ejemplares)
+            List<Ejemplar> ejemplaresDisponibles = new List<Ejemplar>();//Se crea una lista de ejemplares vacia y se guarda en la variable ejemplaresDisponibles
+            foreach (var item in this.Ejemplares)//Recorremos la lista de ejemplares del libro
             {
-                if (item.Disponible && item.Estado == EstadoEjemplar.Bueno)
+                if (item.Disponible && item.Estado == EstadoEjemplar.Bueno)//Verifica si el ejemplar cumple las condiciones de no encontrarse prestado y estar en buen estado
                 {
-                    ejemplaresDisponibles.Add(item);
+                    ejemplaresDisponibles.Add(item);//En caso afirmativo agrega el ejemplar a la lista ejemplaresDisponibles
                 }
 
             }
-            return ejemplaresDisponibles;
+            return ejemplaresDisponibles;//Devuelve la lista ejemplaresDisponibles
         }
 
         public List<Ejemplar> EjemplaresEnBuenEstado()
