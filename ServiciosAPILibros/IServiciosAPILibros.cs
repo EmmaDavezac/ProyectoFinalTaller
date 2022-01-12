3f695f065685537789a14e9c2583519c98172af5
Emmanuel Davezac <89666361+EmmaDavezac@users.noreply.github.com>
2022-01-12 20:12:46 -0300

    Update IServiciosAPILibros.cs

--- ServiciosAPILibros/IServiciosAPILibros.cs
+++ ServiciosAPILibros/IServiciosAPILibros.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 
 namespace ServiciosAPILibros
 {
-    public interface IServiciosAPILibros
+    public interface IServiciosAPILibros//Interface que nos permite buscar informacion sobre libros en una API de libros
     {
-        List<Libro> ListaPorCoincidecia(string cadena);
-        void BuscarPorISBN(string isbn);//hay que corregirlo en APIOpenLibrary
+        List<Libro> ListaPorCoincidecia(string cadena);//Metodo que nos permite obtener una lista de libros que coinciden con el termino buscado
+        
     }
 }
