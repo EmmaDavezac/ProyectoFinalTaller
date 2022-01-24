f2ff183c4653b9213f8607f6b22782001ce206ee
Emmanuel Davezac <89666361+EmmaDavezac@users.noreply.github.com>
2022-01-24 01:17:21 -0300

    comentado

--- Programa/RegistrarAdministrador.cs
+++ Programa/RegistrarAdministrador.cs
@@ -6,15 +6,15 @@ using System.Windows.Forms;
 namespace Programa
 {
     public partial class RegistrarAdministrador : Form
+    //La finalidad de este formulario es la de permitir registrar un nuevo administrador
     {
-        private string nombre { get; set; }
-        private string nombreUsuario { get; set; }
-        FachadaNucleo interfazNucleo = new FachadaNucleo();
-        public RegistrarAdministrador(string pNombreUsuario)
+        private string nombreUsuario { get; set; }//Aqui se almacena el nombre de usuario del administrador que esta usando el programa
+        private FachadaNucleo interfazNucleo = new FachadaNucleo();//Instancia del nucleo del programa que nos permite acceder a las funciones del mismo
+        public RegistrarAdministrador(string pNombreUsuario)//Constructor 
         {
             InitializeComponent();
             nombreUsuario = pNombreUsuario;
-            nombre = interfazNucleo.ObtenerAdministrador(nombreUsuario).Nombre;
+            n
             labelNombreUsuario.Text = "Usuario: " + nombreUsuario;
         }
 
@@ -30,72 +30,73 @@ namespace Programa
 
 
 
-        private void botonVolver_Click(object sender, EventArgs e)
+        private void botonVolver_Click(object sender, EventArgs e)//Se ejecutara cuando se presione el boton volver
         {
 
-            this.Hide();
-            this.Owner.Show();
+            this.Hide();//la ventana se oculta
+            this.Owner.Show();//se muestra la ventana padre
         }
 
-        private void buttonSalir_Click(object sender, EventArgs e)
+        
+
+        private void RegistrarAdministrador_FormClosed(object sender, FormClosedEventArgs e)//se ejecutara cuando se cierre el formulario
         {
-            Application.Exit();
+           this.Hide();//la ventana se oculta
+            this.Owner.Show();//se muestra la ventana padre
         }
 
-        private void RegistrarAdministrador_FormClosed(object sender, FormClosedEventArgs e)
+        private void textBoxNombre_TextChanged(object sender, EventArgs e)//se ejecutara cuando se modifique el texto de textBoxNombre
         {
-            this.Hide();
-            this.Owner.Show();
+            buttonRegistrarAdministrador.Enabled = true;//se activa el boton registrar administrador
         }
 
-        private void textBoxNombre_TextChanged(object sender, EventArgs e)
+        private void textBoxApellido_TextChanged(object sender, EventArgs e)//se ejecutara cuando se modifique el texto de textBoxApellido
         {
-            buttonRegistrarAdministrador.Enabled = true;
+            buttonRegistrarAdministrador.Enabled = true;//se activa el boton registrar administrador
         }
 
-        private void textBoxApellido_TextChanged(object sender, EventArgs e)
+        private void dateTimePickerFechaNacimiento_ValueChanged(object sender, EventArgs e)//se ejecutara cuando se modifique la fecha de ateTimePickerFechaNacimiento
         {
-            buttonRegistrarAdministrador.Enabled = true;
+            buttonRegistrarAdministrador.Enabled = true;//se activa el boton registrar administrador
         }
 
-        private void dateTimePickerFechaNacimiento_ValueChanged(object sender, EventArgs e)
+        private void textBoxMail_TextChanged(object sender, EventArgs e)//se ejecutara cuando se modifique el texto de textBoxMail
         {
-            buttonRegistrarAdministrador.Enabled = true;
+            buttonRegistrarAdministrador.Enabled = true;//se activa el boton registrar administrador
         }
 
-        private void textBoxMail_TextChanged(object sender, EventArgs e)
+        private void textBoxContraseña_TextChanged(object sender, EventArgs e)//se ejecutara cuando se modifique el texto de textBoxContraseña
         {
-            buttonRegistrarAdministrador.Enabled = true;
-        }
-
-        private void textBoxContraseña_TextChanged(object sender, EventArgs e)
-        {
-            buttonRegistrarAdministrador.Enabled = true;
+            buttonRegistrarAdministrador.Enabled = true;//se activa el boton registrar administrador
         }
 
         private void buttonRegistrarAdministrador_Click(object sender, EventArgs e)
+        //se activara cuando se presiona el boton registrar administrador y en el caso de que todos los datos requeridos esten ingresados y sean correctos, se registrara el nuevo administrador
         {
-            if (!string.IsNullOrEmpty(textBoxNombreUsuario.Text))
+            if (!string.IsNullOrEmpty(textBoxNombreUsuario.Text)&&textBoxNombreUsuario.Text.All(Char.IsLetter)))//se verifica que se haya ingresado el nombre de usuario y tenga el formato correcto
             {
-                if (!string.IsNullOrEmpty(textBoxNombre.Text) && textBoxNombre.Text.All(Char.IsLetter))
+                if (!string.IsNullOrEmpty(textBoxNombre.Text) && textBoxNombre.Text.All(Char.IsLetter))//se verifica que se haya ingresado el nombre y tenga el formato correcto
                 {
-                    if (!string.IsNullOrEmpty(textBoxApellido.Text) && textBoxApellido.Text.All(Char.IsLetter))
+                    if (!string.IsNullOrEmpty(textBoxApellido.Text) && textBoxApellido.Text.All(Char.IsLetter))//se verifica que se haya ingresado el apellido y tenga el formato correcto
                     {
-                        if (dateTimePickerFechaNacimiento.Value.Date != new DateTime(1900, 1, 1))
+                        if (dateTimePickerFechaNacimiento.Value.Date != new DateTime(1900, 1, 1))//se verifica que se haya ingresado la fecha de nacimiento
                         {
-                            if (!string.IsNullOrEmpty(textBoxMail.Text) && interfazNucleo.EsUnEmailValido(textBoxMail.Text))
+                            if (!string.IsNullOrEmpty(textBoxMail.Text) && interfazNucleo.EsUnEmailValido(textBoxMail.Text))//se verifica que se haya ingresado el mail y tenga el formato correcto
                             {
                                 if (!string.IsNullOrEmpty(textBoxTelefono.Text) && textBoxTelefono.Text.All(Char.IsDigit) && textBoxTelefono.Text.Length >= 8 && textBoxTelefono.Text.Length <= 11)
+                                //se verifica que se haya ingresado numero de telefono y tenga el formato correcto
                                 {
                                     if (!string.IsNullOrEmpty(textBoxContraseña.Text) && textBoxContraseña.Text.Length >= 4)
+                                    //se verifica que se haya ingresado la contraseña y tenga el formato correcto
                                     {
                                         bool resultado = interfazNucleo.AñadirAdministrador(textBoxNombreUsuario.Text, textBoxNombre.Text, textBoxApellido.Text, dateTimePickerFechaNacimiento.Value, textBoxMail.Text, textBoxContraseña.Text, textBoxTelefono.Text);
+                                        //se intenta registrar el nuevo administrador y notifica en pantalla si la operacion fue exitosa o no
                                         if (resultado == true)
                                         {
                                             MessageBox.Show("Usuario administrador guardado, el nombre de usuario es: " + textBoxNombreUsuario.Text, "Operacion Exitosa", MessageBoxButtons.OK);
 
-                                            this.Hide();
-                                            this.Owner.Show();
+                                            this.Hide();//se oculta la ventana actual
+                                            this.Owner.Show();// se vuelve a la ventana padre
                                         }
                                         else
                                         {
@@ -156,39 +157,39 @@ namespace Programa
             }
         }
 
-        private void dateTimePickerFechaNacimiento_ValueChanged_1(object sender, EventArgs e)
+        private void dateTimePickerFechaNacimiento_ValueChanged_1(object sender, EventArgs e)// se ejecutara cuando se cambie la fecha de dateTimePickerFechaNacimiento
         {
-            buttonRegistrarAdministrador.Enabled = true;
+            buttonRegistrarAdministrador.Enabled = true;//se activa el boton registrar administrador
             labelError.Text = "";
         }
 
-        private void textBoxNombre_TextChanged_1(object sender, EventArgs e)
+        private void textBoxNombre_TextChanged_1(object sender, EventArgs e)//se ejecutara cuando se modifique el texto de textBoxNombre
         {
-            buttonRegistrarAdministrador.Enabled = true;
+            buttonRegistrarAdministrador.Enabled = true;//se activa el boton registrar administrador
             labelError.Text = "";
         }
 
-        private void textBoxApellido_TextChanged_1(object sender, EventArgs e)
+        private void textBoxApellido_TextChanged_1(object sender, EventArgs e)//se ejecutara cuando se modifique el texto de textBoxApellido
         {
-            buttonRegistrarAdministrador.Enabled = true;
+            buttonRegistrarAdministrador.Enabled = true;//se activa el boton registrar administrador
             labelError.Text = "";
         }
 
-        private void textBoxMail_TextChanged_1(object sender, EventArgs e)
+        private void textBoxMail_TextChanged_1(object sender, EventArgs e)//se ejecutara cuando se modifique el texto de textBoxMail
         {
-            buttonRegistrarAdministrador.Enabled = true;
+            buttonRegistrarAdministrador.Enabled = true;//se activa el boton registrar administrador
             labelError.Text = "";
         }
 
-        private void textBoxTelefono_TextChanged(object sender, EventArgs e)
+        private void textBoxTelefono_TextChanged(object sender, EventArgs e)//se ejecutara cuando se modifique el texto de textBoxTelefono
         {
-            buttonRegistrarAdministrador.Enabled = true;
+            buttonRegistrarAdministrador.Enabled = true;//se activa el boton registrar administrador
             labelError.Text = "";
         }
 
-        private void textBoxContraseña_TextChanged_1(object sender, EventArgs e)
+        private void textBoxContraseña_TextChanged_1(object sender, EventArgs e)//se ejecutara cuando se modifique el texto de textBoxContraseña
         {
-            buttonRegistrarAdministrador.Enabled = true;
+            buttonRegistrarAdministrador.Enabled = true;//se activa el boton registrar administrador
             labelError.Text = "";
         }
 
@@ -197,9 +198,10 @@ namespace Programa
 
         }
 
-        private void textBoxNombreUsuario_TextChanged(object sender, EventArgs e)
+        private void textBoxNombreUsuario_TextChanged(object sender, EventArgs e)//se ejecutara cuando se modifique el texto de textBoxNombreUsuario
         {
-
+        buttonRegistrarAdministrador.Enabled = true;//se activa el boton registrar administrador
+            labelError.Text = "";
         }
 
         private void label2_Click_1(object sender, EventArgs e)
@@ -227,7 +229,7 @@ namespace Programa
 
         }
 
-        private void buttonMostrar_Click(object sender, EventArgs e)
+        private void buttonMostrar_Click(object sender, EventArgs e)//Se ejecutara cuando se presione el boton para mostrar u ocultar la contraseña
         {
             if (textBoxContraseña.UseSystemPasswordChar == true)
             {
