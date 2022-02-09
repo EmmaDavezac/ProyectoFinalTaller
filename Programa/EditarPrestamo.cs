d0439fa42b923cbb83a542f85da59887377e7fdf
Emmanuel Davezac <89666361+EmmaDavezac@users.noreply.github.com>
2022-02-09 16:39:30 -0300

    try catch

--- Programa/EditarPrestamo.cs
+++ Programa/EditarPrestamo.cs
@@ -1,11 +1,11 @@
-﻿using Nucleo;
+using Nucleo;
 using System;
 using System.Windows.Forms;
 
 namespace Programa
 {
     public partial class EditarPrestamo : Form
-    /*Este formulario tiene la finalidad de permitir la edicion de un presamo(estara disponible solo para versiones de prueba, 
+    /*Este formulario tiene la finalidad de permitir la edicion de un prestamo(estara disponible solo para versiones de prueba, 
     para demostrar el buen funcionamiento del programa)*/
     {
         private int idPrestamo;//Atributo que almacena el id de un prestamo
@@ -23,9 +23,18 @@ namespace Programa
 
         private void buttonModificarFechas_Click(object sender, EventArgs e)//Este evento se ejecuta cuando se presiona el boton buttonModificarFechas
         {
+           try
+           {
             interfazNucleo.ModificarFechasPrestamo(idPrestamo, textBoxFechaPrestamo.Text, textBoxFechaLimite.Text);//Se modifican las fechas del prestamo
             ((GestionarPrestamos)this.Owner).ObtenerPrestamos();//Se actualiza la lista de prestamos en el formulario padre
             this.Close();//Se cierra este formulario y se vuelve al formulario padre
+           }
+           catch (Exception ex)
+            {
+                string texto= "Error buttonModificarFechas_Click: "+ ex.Message + ex.StackTrace;
+                interfazNucleo.RegistrarLog(texto,"Ha ocurrido un error");
+                MessageBox.Show();
+            }
         }
 
         private void buttonVolver_Click(object sender, EventArgs e)//Este evento se ejecuta cuando se presiona el boton buttonVolver
@@ -35,9 +44,18 @@ namespace Programa
 
         internal void InicialiarEditarPrestamo(int pIdPrestamo, string pFechaPrestamo, string pFechaLimite)
         {
+            try
+            {
             textBoxFechaPrestamo.Text = pFechaPrestamo;
             textBoxFechaLimite.Text = pFechaLimite;
             idPrestamo = pIdPrestamo;
+            }
+            catch (Exception ex)
+            {
+                string texto= "Error InicialiarEditarPrestamo: "+ ex.Message + ex.StackTrace;
+                interfazNucleo.RegistrarLog(texto,"Ha ocurrido un error");
+                MessageBox.Show();
+            }
         }
     }
 }
